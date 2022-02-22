ea92cc326128b81b7c15adcbae26495e836b8c02
110819016 <71056572+110819016@users.noreply.github.com>
2022-02-22 14:07:01 +0800

    init

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>110819016</Text>
+      <Text style={styles.text}>許靖宜</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +14,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f0dfe4',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  text: {
+    color: 'rgba(104,26,26,0.89)',
+    fontSize: 40,
+  }
 });
